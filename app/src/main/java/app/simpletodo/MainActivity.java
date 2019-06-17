fcdfd85744c0a16775ffdc6a96917e60e73db1f2
Miguel <miguelzamudio.dev@hotmail.com>
2019-06-17 02:48:25 -0600

    Implement add and delete item

--- app/src/main/java/app/simpletodo/MainActivity.java
+++ app/src/main/java/app/simpletodo/MainActivity.java
@@ -2,12 +2,31 @@ package app.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv_items;
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv_items = findViewById(R.id.lv_items);
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        //Mock data
+        items.add("First Item");
+        items.add("Second Item");
+
     }
 }
