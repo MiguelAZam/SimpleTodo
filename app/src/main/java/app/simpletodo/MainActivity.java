365298b6ea511b659a762300e8dfb5c7fa95fc25
Miguel <miguelzamudio.dev@hotmail.com>
2019-07-02 21:20:35 -0600

    Reset app functionality and modify Main Activity layout

--- app/src/main/java/app/simpletodo/MainActivity.java
+++ app/src/main/java/app/simpletodo/MainActivity.java
@@ -12,23 +12,18 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     //Numberic code to identify the edit activity
+    public final static int CREATE_REQUEST_CODE = 10;
     public final static int EDIT_REQUEST_CODE = 20;
 
     //Keys used for passing data between activities
     public final static String ITEM_TEXT = "itemText";
     public final static String ITEM_POSITION = "itemPosition";
 
-    ListView lv_items;
 
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
@@ -37,85 +32,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        lv_items = findViewById(R.id.lv_items);
-
-        readItems();
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        lv_items.setAdapter(itemsAdapter);
-
-        setupListViewListener();
-
-    }
-
-    public void onAddItem(View v){
-        EditText et_new_item = findViewById(R.id.et_new_item);
-        String newItem = et_new_item.getText().toString();
-        itemsAdapter.add(newItem);
-        et_new_item.setText("");
-
-        writeItems();
-
-        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
-    }
-
-    public void setupListViewListener(){
-        lv_items.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                items.remove(position);
-                itemsAdapter.notifyDataSetChanged();
-                writeItems();
-                return true;
-            }
-        });
-
-        lv_items.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
-                intent.putExtra(ITEM_TEXT, items.get(position));
-                intent.putExtra(ITEM_POSITION, position);
-
-                startActivityForResult(intent, EDIT_REQUEST_CODE);
-            }
-        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE){
+        /*if(resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE){
             String updateItem = data.getExtras().getString(ITEM_TEXT);
             int position = data.getExtras().getInt(ITEM_POSITION);
             items.set(position, updateItem);
             itemsAdapter.notifyDataSetChanged();
-            writeItems();
 
             Toast.makeText(this, "Item updated successfully", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public File getDataFile(){
-        return new File(getFilesDir(), "todo.txt");
-    }
-
-    public void readItems(){
-        try{
-            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
-        } catch(IOException e){
-            Log.e("Main Activity", "Error reading file", e);
-            items = new ArrayList<>();
-        }
-
-    }
-
-    public void writeItems(){
-        try{
-            FileUtils.writeLines(getDataFile(), items);
-        } catch(IOException e){
-            Log.e("Main Activity", "Error writing items", e);
-        }
-
+        }*/
     }
 }
